4784f9543627753b77385fa12108f87c8e069cb0
KIETMIO <108475494+KIETMIO@users.noreply.github.com>
2024-01-07 15:24:57 +0700

    Add files via upload

---
+++ AnhKietHub.lua
@@ -0,0 +1,4 @@
+
+_G.Name = "Anh Kiet Hub" -- Put You Name Script Here
+_G.Logo = "https://mcdn.coolmate.me/image/January2024/anh-meo-meme-tet-3543_869.jpg" -- Enter Your ID Image Here
+loadstring(Game:HttpGet"https://raw.githubusercontent.com/ImTienNguyenZ/NguyenTienSimpRose/main/NguyenMinhTienLoader")()
